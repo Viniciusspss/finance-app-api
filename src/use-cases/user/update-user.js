346cc1e54674baca030a2e8e9224f2274d212964
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2026-09-03 20:51:38 -0300

    feat: creates update user use case

---
+++ src/use-cases/user/update-user.js
@@ -0,0 +1,43 @@
+import { EmailAlreadyInUseError } from '../../errors/user.js'
+
+export class UpdateUserUseCase {
+    constructor(
+        getUserByEmailRepository,
+        updateUserRepository,
+        passwordHasherAdapter,
+    ) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.updateUserRepository = updateUserRepository
+        this.passwordHasherAdapter = passwordHasherAdapter
+    }
+    async execute(userId, updateUserParams) {
+        // se o email tiver sendo atualizado, verifica-lo se ja esta em uso
+
+        if (updateUserParams.email) {
+            const userWithProvidedEmail =
+                await this.getUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProvidedEmail && userWithProvidedEmail.id !== userId) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await this.passwordHasherAdapter.execute(
+                updateUserParams.password,
+            )
+
+            user.password = hashedPassword
+        }
+
+        const updateUser = await this.updateUserRepository.execute(userId, user)
+
+        return updateUser
+    }
+}
