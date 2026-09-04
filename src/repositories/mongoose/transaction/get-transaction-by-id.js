877538b8d63e0e4f676ce46ae8cb4520e4810417
Santosz777 <danilosantosaraujo03@gmail.com>
2026-09-03 21:33:49 -0300

    feat: add get transaction by id repository

---
+++ src/repositories/mongoose/transaction/get-transaction-by-id.js
@@ -0,0 +1,10 @@
+import { TransactionModel } from '../../models/Transaction.js'
+import { toDocument } from '../../utils/decimal.js'
+
+export class MongooseGetTransactionByIdRepository {
+    async execute(transactionId) {
+        const transaction = await TransactionModel.findOne({ id: transactionId })
+
+        return toDocument(transaction)
+    }
+}
