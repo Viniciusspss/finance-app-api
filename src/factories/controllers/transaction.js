a368918fd022af5a80206176edb13e6712174400
cord31ro <contapserv@gmail.com>
2026-09-03 21:52:29 -0300

    feat: add makeUpdateTransactionController factory

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,16 +1,19 @@
 import {
     CreateTransactionController,
     DeleteTransactionController,
+    UpdateTransactionController,
 } from '../../controller/index.js'
 import {
     MongooseCreateTransactionRepository,
     MongooseGetUserByIdRepository,
     MongooseDeleteTransactionRepository,
     MongooseGetTransactionByIdRepository,
+    MongooseUpdateTransactionRepository,
 } from '../../repositories/mongoose/index.js'
 import {
     CreateTransactionUseCase,
     DeleteTransactionUseCase,
+    UpdateTransactionUseCase,
 } from '../../use-cases/index.js'
 
 import { IdGeneratorAdapter } from '../../adapters/index.js'
@@ -53,4 +56,23 @@ export const makeDeleteTransactionController = () => {
     )
 
     return deleteTransactionController
+}
+
+export const makeUpdateTransactionController = () => {
+    const updateTransactionRepository =
+        new MongooseUpdateTransactionRepository()
+
+    const getTransactionByIdRepository =
+        new MongooseGetTransactionByIdRepository()
+
+    const updateTransactionUseCase = new UpdateTransactionUseCase(
+        updateTransactionRepository,
+        getTransactionByIdRepository,
+    )
+
+    const updateTransactionController = new UpdateTransactionController(
+        updateTransactionUseCase,
+    )
+
+    return updateTransactionController
 }
