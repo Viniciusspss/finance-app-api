b0a61685be168bf41cc7a548cf0b0b5e98b28ceb
cord31ro <contapserv@gmail.com>
2026-09-04 15:12:45 -0300

    docs: add swagger docs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import express from 'express'
+import cors from 'cors'
+import { usersRouter, transactionsRouter } from './routes/index.js'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-import { usersRouter } from './routes/index.js'
-import { transactionsRouter } from './routes/index.js'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
+app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
 
 app.use('/api/users', usersRouter)
 app.use('/api/transactions', transactionsRouter)
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(join(__dirname, '../docs/swagger.json'), 'utf-8'),
+)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 export { app }
