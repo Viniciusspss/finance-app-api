0b33e4a7b53a882bfb7a8a3effed8bd0548a01f5
Santosz777 <danilosantosaraujo03@gmail.com>
2026-09-03 21:22:24 -0300

    feat: add transaction validation schema with zod

---
+++ src/schemas/transaction.js
@@ -0,0 +1,59 @@
+import { z } from 'zod'
+import validator from 'validator'
+
+export const createTransactionSchema = z.object({
+    user_id: z
+        .string({
+            required_error: 'User id is required',
+        })
+        .uuid({
+            message: 'User id must be a valid uuid',
+        }),
+    name: z
+        .string({
+            required_error: 'Name is required',
+        })
+        .trim()
+        .min(1, {
+            message: 'Name is required',
+        }),
+    date: z
+        .string({
+            required_error: 'Date is required',
+        })
+        .datetime({
+            message: 'Date must be a valid date',
+        }),
+    type: z.enum(['EARNING', 'EXPENSE', 'INVESTMENT'], {
+        errorMap: () => ({
+            message: 'The type must be: EARNING, EXPENSE, INVESTMENT',
+        }),
+    }),
+    amount: z
+        .number({
+            required_error: 'The amount is required',
+            invalid_type_error: 'Amount must be a number',
+        })
+        .min(1, {
+            message: 'Amount must be greater than 0',
+        })
+        .refine((value) =>
+            validator.isCurrency(value.toFixed(2), {
+                digits_after_decimal: [2],
+                allow_negatives: false,
+                decimal_separator: '.',
+            }),
+        ),
+})
+
+export const updateTransactionSchema = createTransactionSchema
+    .omit({
+        user_id: true,
+    })
+    .partial()
+
+export const getTransactionsByUserIdSchema = z.object({
+    user_id: z.string().uuid(),
+    from: z.string().date(),
+    to: z.string().date(),
+})
