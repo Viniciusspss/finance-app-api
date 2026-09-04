fb12797d6129315c40d9922002d80ad435f85fe9
cord31ro <contapserv@gmail.com>
2026-09-03 21:49:40 -0300

    feat: add update transaction use case

---
+++ src/use-cases/transaction/update-transaction.js
@@ -0,0 +1,20 @@
+import { ForbiddenError } from '../../errors/index.js'
+
+export class UpdateTransactionUseCase {
+    constructor(updateTransactionRepository, getTransactionByIdRepository) {
+        this.updateTransactionRepository = updateTransactionRepository
+        this.getTransactionByIdRepository = getTransactionByIdRepository
+    }
+    async execute(transactionId, params) {
+        const transaction =
+            await this.getTransactionByIdRepository.execute(transactionId)
+
+        if (params.user_id && transaction.user_id !== params.user_id) {
+            throw new ForbiddenError()
+        }
+        return await this.updateTransactionRepository.execute(
+            transactionId,
+            params,
+        )
+    }
+}
