ac3e56b3daa0ed7f4f0cf09af9146d55dfee0156
cord31ro <contapserv@gmail.com>
2026-09-03 21:54:41 -0300

    feat: add update transaction route

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,6 +1,10 @@
 import { Router } from 'express'
 
-import { makeCreateTransactionController } from '../factories/controllers/transaction.js'
+import {
+    makeCreateTransactionController,
+    makeDeleteTransactionController,
+    makeUpdateTransactionController,
+} from '../factories/controllers/transaction.js'
 import { auth } from '../middlewares/auth.js'
 
 export const transactionsRouter = Router()
@@ -9,6 +13,20 @@ transactionsRouter.post('/me', auth, async (req, res) => {
     const createTransactionController = makeCreateTransactionController()
 
     const { statusCode, body } = await createTransactionController.execute({
+        ...req,
+        body: {
+            ...req.body,
+            user_id: req.userId,
+        },
+    })
+
+    res.status(statusCode).send(body)
+})
+
+transactionsRouter.patch('/me/:transactionId', auth, async (req, res) => {
+    const updateTransactionController = makeUpdateTransactionController()
+
+    const { statusCode, body } = await updateTransactionController.execute({
         ...req,
         body: {
             ...req.body,
